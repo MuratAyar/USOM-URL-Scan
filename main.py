28bc95bc92bfad801fff278fefe3ba56ab49f55a
MuratAyar <ayar.murat55@gmail.com>
2022-07-04 09:38:31 +0300

    Update main.py
    -Now the application works only if someone send request
    -Request has to include a URL to be scanned.
    -Better visibility.

--- main.py
+++ main.py
@@ -4,105 +4,85 @@ import time
 import urllib.request
 import urllib
 import os
-from decouple import config
-from requests.adapters import HTTPAdapter, Retry
+from flask import Flask, Response, request
 
-URL_TO_BE_SCANNED= config('URL_TO_BE_SCANNED')
-URLSCAN_TOKEN = config('URLSCAN_TOKEN')
-TIME_ENV= config('TIME_ENV', default=10, cast=int)
+app = Flask(__name__)
 
+URLSCANIO_API_KEY = os.getenv('URLSCANIO_API_KEY')
+SLEEP_TIME = os.getenv('SLEEP_TIME')
+RETRY = os.getenv('RETRY')
 
-headers = {'API-Key': URLSCAN_TOKEN, 'Content-Type': 'application/json'}
-data = {"url": URL_TO_BE_SCANNED, "visibility": "public"}
-response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
-time.sleep(TIME_ENV)
+@app.route('/')
+def hello():
 
+    url = request.args["url"]
 
-data = json.dumps(response.json()['api'])
-api_url = data[1:-1]
+    if (url == ""):
+        return Response(json.dumps('{success: false, "message": "Hatalı URL"}'))
 
-json_data = urllib.request.urlopen(api_url)
-db = json.loads(json_data.read())
+    headers = {'API-Key': URLSCANIO_API_KEY, 'Content-Type': 'application/json'}
+    data = {"url": url, "visibility": "public"}
+    response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
+    
+    for x in range(RETRY):
+        time.sleep(10)
 
-#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        data = json.dumps(response.json()['api'])
+        api_url = data[1:-1]
 
-screenshotURL = db.get('task').get('screenshotURL')  #-----------------------
-file_name = screenshotURL.split("/")[-1]
+        json_data = urllib.request.urlopen(api_url)
+        db = json.loads(json_data.read())
 
+        return Response(json.dumps('{success: true, "data": '+db+'}'), mimetype='application/json')
 
-def download_image(url, filename):                   #SCREENSHOT
-    #full_path = filepath + filename + '.jpg'
-    urllib.request.urlretrieve(url, filename)
+    return Response(json.dumps('{success: false, "message": "Server Zaman Aşımına Uğradı"}'))
 
+def parseJSON(db):
+    screenshotURL = db.get('task').get('screenshotURL')  
+    file_name = screenshotURL.split("/")[-1]
 
-#download_image(screenshotURL, file_name)              #--------------------------
+    submitter_country = db.get('submitter').get('country')
 
-submitter_country = db.get('submitter').get('country')
+    overall_verdicts = db.get('verdicts').get('overall').get('score')           
+    overall_malicious = db.get('verdicts').get('overall').get('malicious')
+    overall_hasverdicts = db.get('verdicts').get('overall').get('hasVerdicts')
+    verdict_categories = db.get('verdicts').get('overall').get('categories')
+    verdict_brands = db.get('verdicts').get('overall').get('brands')
+    verdict_tags = db.get('verdicts').get('overall').get('tags')                            
+    if overall_hasverdicts == True:
+        potentially_malicious = overall_malicious
+    else:
+        potentially_malicious = "UNDETECTED"                                     
 
-overall_verdicts = db.get('verdicts').get('overall').get('score')           #--------------------------------------------------
-overall_malicious = db.get('verdicts').get('overall').get('malicious')
-overall_hasverdicts = db.get('verdicts').get('overall').get('hasVerdicts')
-verdict_categories = db.get('verdicts').get('overall').get('categories')
-verdict_brands = db.get('verdicts').get('overall').get('brands')
-verdict_tags = db.get('verdicts').get('overall').get('tags')                            #VERDICTS
-if overall_hasverdicts == True:
-    potentially_malicious = overall_malicious
-else:
-    potentially_malicious = "UNDETECTED"                                     #----------------------------------------------------
+    page_ip = db.get('page').get('ip')
+    page_domain = db.get('page').get('domain')
+    page_country = db.get('page').get('country')
+    page_city = db.get('page').get('city')
 
-page_ip = db.get('page').get('ip')
-page_domain = db.get('page').get('domain')
-page_country = db.get('page').get('country')
-page_city = db.get('page').get('city')
+    report_URL = db.get('task').get('reportURL')
 
-report_URL = db.get('task').get('reportURL')
+    geo_ip_datas = {}                                                                        
+    geo_data_list = db.get('meta').get('processors').get('geoip').get('data')
+    for n in geo_data_list:
+        ip = n.get('ip')
+        if ip == page_ip:
+            geoip_data = n.get('geoip')
+                                                                  
 
-geo_ip_datas = {}                                                                        #---------------------------------------------------
-geo_data_list = db.get('meta').get('processors').get('geoip').get('data')
-for n in geo_data_list:
-    ip = n.get('ip')
-    if ip == page_ip:
-        geoip_data = n.get('geoip')
+    rdns_data= db.get('meta').get('processors').get('rdns').get('data')
 
-
-#    country_name = n.get('geoip').get('country_name')                                               #GEOIP DATA LIST
-#    time_zone = n.get('geoip').get('timezone')
-#    city_name = n.get('geoip').get('city')
-
-#    geo_ip_datas[ip] = str("[Country Name: " + country_name + "] [Timezone: " + time_zone + "] [City: " + city_name + "]")
-                                                                                        #-------------------------------------------------------
-
-rdns_data= db.get('meta').get('processors').get('rdns').get('data')
-
-#print("screenshotURL= " + screenshotURL)
-#print("reportURL= " + report_URL)
-#print("ip= " + page_ip)
-#print("Potentially Malicious= " + str(potentially_malicious))
-#print(db.get('verdicts').get('overall'))
-#print(db.get('verdicts').get('urlscan'))
-#print(geoip_data)
-#print(rdns_data)
-
-final_json_data = {}
-final_json_data ={
-    "data":{
-        "ScreenShotURL": screenshotURL,
-        "ReportURL": report_URL,
-        "IP": page_ip,
-        "Potentially_Malicious": potentially_malicious,
-        "Overall_Verdicts": db.get('verdicts').get('overall'),
-        "URLScan_Verdicts": db.get('verdicts').get('urlscan'),
-     "GeoIP_Data": geoip_data,
-        "RDNS_Data": rdns_data,
+    final_json_data = {}
+    final_json_data ={
+        "data":{
+            "ScreenShotURL": screenshotURL,
+            "ReportURL": report_URL,
+            "IP": page_ip,
+            "Potentially_Malicious": potentially_malicious,
+            "Overall_Verdicts": db.get('verdicts').get('overall'),
+            "URLScan_Verdicts": db.get('verdicts').get('urlscan'),
+            "GeoIP_Data": geoip_data,
+            "RDNS_Data": rdns_data,
+        }
     }
-}
-print(json.dumps(final_json_data))
 
-
-#requests.post("http://localhost", verify=False)
-
-
-with open('testdata.json', 'w', encoding='utf-8') as f:
-    json.dump(final_json_data, f, ensure_ascii=False, indent=4)
-
-os.system("json-server --watch testdata.json --port 8000")
+    return final_json_data
