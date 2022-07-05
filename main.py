ff60b6e36422c1144ed0fc5ae9b6b7e7f1366e93
MuratAyar <ayar.murat55@gmail.com>
2022-07-05 14:17:29 +0300

    Test-Kule Latest version

--- main.py
+++ main.py
@@ -1,86 +1,55 @@
-import requests
 import json
-import time
-import urllib.request
-import urllib
 import os
+import requests
+import time
+import urllib
+import urllib.request
 from flask import Flask, Response, request
+
 app = Flask(__name__)
+
 URLSCANIO_API_KEY = os.getenv('URLSCANIO_API_KEY')
-SLEEP_TIME = os.getenv('SLEEP_TIME', default=10, cast=int)
-RETRY = os.getenv('RETRY')
+SLEEP_TIME = os.getenv('SLEEP_TIME', default=3)
+RETRY = os.getenv('RETRY', default=5)
+print(URLSCANIO_API_KEY)
 
 @app.route('/')
 def hello():
+    if 'url' not in request.args:
+        return get_error_response("url parametresi verilmedi")
 
     url = request.args["url"]
 
-    if (url == ""):
-        return Response(json.dumps('{success: false, "message": "Hatalı URL"}'))
 
     headers = {'API-Key': URLSCANIO_API_KEY, 'Content-Type': 'application/json'}
     data = {"url": url, "visibility": "public"}
-    response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
-    
+    response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data)).json()
+
+    if 'api' not in response:
+        return get_error_response(response['message'])
+
+    data = json.dumps(response['api'])
+    api_url = data[1:-1]
+
     for x in range(RETRY):
-        time.sleep(10)
+        time.sleep(SLEEP_TIME)
 
-        data = json.dumps(response.json()['api'])
-        api_url = data[1:-1]
+        try:
+            json_data = urllib.request.urlopen(api_url)
+            db = json.loads(json_data.read())
 
-        json_data = urllib.request.urlopen(api_url)
-        db = json.loads(json_data.read())
+            return get_succes_response(db)
+        except:
+            pass
 
-        return Response(json.dumps('{success: true, "data": '+db+'}'), mimetype='application/json')
+    return get_error_response("Server Zaman Aşımına Uğradı")
 
-    return Response(json.dumps('{success: false, "message": "Server Zaman Aşımına Uğradı"}'))
+def get_error_response(msg):
+    return Response('{"success": false, "message": "' + msg + '"}', mimetype='application/json')
 
-def parseJSON(db):
-    screenshotURL = db.get('task').get('screenshotURL')  
-    file_name = screenshotURL.split("/")[-1]
 
-    submitter_country = db.get('submitter').get('country')
+def get_succes_response(data):
+    return Response('{"success": true, "data": ' + json.dumps(data) + '}', mimetype='application/json')
 
-    overall_verdicts = db.get('verdicts').get('overall').get('score')           
-    overall_malicious = db.get('verdicts').get('overall').get('malicious')
-    overall_hasverdicts = db.get('verdicts').get('overall').get('hasVerdicts')
-    verdict_categories = db.get('verdicts').get('overall').get('categories')
-    verdict_brands = db.get('verdicts').get('overall').get('brands')
-    verdict_tags = db.get('verdicts').get('overall').get('tags')                            
-    if overall_hasverdicts == True:
-        potentially_malicious = overall_malicious
-    else:
-        potentially_malicious = "UNDETECTED"                                     
-
-    page_ip = db.get('page').get('ip')
-    page_domain = db.get('page').get('domain')
-    page_country = db.get('page').get('country')
-    page_city = db.get('page').get('city')
-
-    report_URL = db.get('task').get('reportURL')
-
-    geo_ip_datas = {}                                                                        
-    geo_data_list = db.get('meta').get('processors').get('geoip').get('data')
-    for n in geo_data_list:
-        ip = n.get('ip')
-        if ip == page_ip:
-            geoip_data = n.get('geoip')
-                                                                  
-
-    rdns_data= db.get('meta').get('processors').get('rdns').get('data')
-
-    final_json_data = {}
-    final_json_data ={
-        "data":{
-            "ScreenShotURL": screenshotURL,
-            "ReportURL": report_URL,
-            "IP": page_ip,
-            "Potentially_Malicious": potentially_malicious,
-            "Overall_Verdicts": db.get('verdicts').get('overall'),
-            "URLScan_Verdicts": db.get('verdicts').get('urlscan'),
-            "GeoIP_Data": geoip_data,
-            "RDNS_Data": rdns_data,
-        }
-    }
-
-    return final_json_data
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
